65b99f1acb8d98646bf9ac197109db01efc3c76c
xFluke <mikoman1124@gmail.com>
2020-12-09 23:56:36 -0500

    Added Game Saver Class

---
+++ Assets/_Scripts/GameSaver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameSaver : MonoBehaviour
+{
+    public static UnityEvent OnSave = new UnityEvent();
+    public static UnityEvent OnLoad = new UnityEvent();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            OnSave.Invoke();
+        }
+    }
+}
