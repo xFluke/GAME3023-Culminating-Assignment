3bb9ffc1e31bee976bd10d650d1f270c45f35622
sh960440 <54958110+sh960440@users.noreply.github.com>
2020-12-12 11:02:50 +0800

    Health bar

---
+++ Assets/_Scripts/BattleSystem/BattlingCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattlingCharacter : MonoBehaviour
+{   
+    int health;
+
+    public int maxHealth;
+
+    public int Health {
+        get { return health; }
+        set { health = value; }
+    }
+    
+    [SerializeField]
+    Ability[] abilities;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void SetAbilities(Ability[] _abilities) {
+        abilities = _abilities;
+    }
+
+    public Ability GetAbilityAtIndex(int index) {
+        return abilities[index];
+    }
+}
