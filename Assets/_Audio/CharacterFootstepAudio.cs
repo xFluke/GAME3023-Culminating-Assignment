9a30bc9b25981127b4d743eeade93834ec1f3a2c
sh960440 <54958110+sh960440@users.noreply.github.com>
2020-12-12 20:24:58 +0800

    Footstep and tilemap

---
+++ Assets/_Audio/CharacterFootstepAudio.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFootstepAudio : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip[] footstepSounds;
+    
+    [SerializeField]
+    AudioSource footstepSource;
+    
+    [SerializeField]
+    PlayerController player;
+
+    [Tooltip("Variance with which to pitch footsteps up/down both ways from 1")]
+    [SerializeField]
+    float pitchVariance = 0.5f;
+
+    public void PlayFootstep()
+    {
+        if (player.isWalking)
+        {
+            switch (player.groundType)
+            {
+                case GroundType.GRASS:
+                    footstepSource.clip = footstepSounds[(int)GroundType.GRASS];
+                    break;
+            
+                case GroundType.ROAD:
+                    footstepSource.clip = footstepSounds[(int)GroundType.ROAD];
+                    break;
+            
+                case GroundType.TALLGRASS:
+                    footstepSource.clip = footstepSounds[(int)GroundType.TALLGRASS];
+                    break;
+
+                default:
+                    footstepSource.clip = footstepSounds[0];
+                    break;
+            }
+        
+            footstepSource.pitch = Random.Range(1.0f - pitchVariance, 1.0f + pitchVariance);
+            footstepSource.Play();
+        }
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GroundType
+{
+    GRASS,
+    ROAD,
+    TALLGRASS
+}
 public class PlayerController : MonoBehaviour
 {
+    public GroundType groundType;
+
     [SerializeField]
     float speed = 5;
 
     Rigidbody2D rb;
 
+    public bool isWalking = false;
+
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.tag)
+        {
+            case "Grass":
+                groundType = GroundType.GRASS;
+                break;
+            case "Road":
+                groundType = GroundType.ROAD;
+                break;
+            case "TallGrass":
+                groundType = GroundType.TALLGRASS;
+                break;
+            default:
+                groundType = GroundType.GRASS;
+                break;
+        }
     }
 
     void Update()
@@ -18,5 +47,14 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         movementVector *= speed;
         rb.velocity = movementVector;
+
+        if (movementVector.magnitude > 0.1f)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
     }
 }
